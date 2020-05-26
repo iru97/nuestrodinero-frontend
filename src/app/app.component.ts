72838f55169c09e3ff6b9db009205860636236e7
franjpr <franpr90@gmail.com>
2020-05-26 22:04:30 +0100

    added seo tags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,26 +9,54 @@ import {
   AfterViewChecked,
 } from '@angular/core';
 import { BoeService } from './services/boe.service';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit, OnDestroy, AfterViewChecked {
+export class AppComponent implements OnInit, OnDestroy {
   isLoading: boolean = true;
   loadingSubscription: Subscription;
   isLoadingObserver: PartialObserver<any[]>;
   ads$: Observable<Documento[]>;
-  @ViewChild('#bmc-wbtn', { static: false }) imgElement;
 
-  constructor(private boeService: BoeService, private renderer: Renderer2) {}
+  constructor(
+    private boeService: BoeService,
+    private title: Title,
+    private meta: Meta
+  ) {
+    this.initMetatags();
+  }
 
   ngOnInit(): void {
-    this.changeBMCIcon();
     let today = this.getDateFormat();
     this.ads$ = this.boeService.getAds(today);
     this.loadingSubscription = this.ads$.subscribe(this.initObserver());
+  }
+
+  initMetatags(): void {
+    this.title.setTitle('Contratos públicos');
+    this.meta.addTags([
+      {
+        name: 'og:title',
+        content: 'Contratos públicos',
+      },
+      {
+        name: 'og:description',
+        content:
+          'Queremos mostrar cuánto dinero público va destinado a las empresas y por qué.',
+      },
+      {
+        name: 'og:url',
+        content: '/',
+      },
+      {
+        name: 'og:image',
+        content: '/assets/images/nuestrodinero_icon.png',
+      },
+    ]);
   }
 
   initObserver(): PartialObserver<any[]> {
@@ -47,15 +75,6 @@ export class AppComponent implements OnInit, OnDestroy, AfterViewChecked {
     let isoDate: string = isoDateTime.substring(0, tIndex);
 
     return isoDate.replace(/-/g, '');
-  }
-
-  changeBMCIcon(): void {
-    //this.renderer.selectRootElement();
-  }
-
-  ngAfterViewChecked(): void {
-    // let item = document.querySelector('#bmc-wbtn');
-    // console.log(item);
   }
 
   ngOnDestroy(): void {

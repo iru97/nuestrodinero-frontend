53bd4881bec9788007f6e89582e12c0ab688582a
franjpr <franpr90@gmail.com>
2020-06-10 19:23:51 +0100

    deply test

--- functions/copy.js
+++ functions/copy.js
@@ -1,9 +1,10 @@
-const fs = require('fs-extra');
+const fs = require('fs');
 
 (async () => {
   const src = '../dist/nuestrodinero-frontend';
   const dest = './dist/nuestrodinero-frontend';
 
-  await fs.remove(dest);
-  await fs.copy(src, dest);
+  await fs.unlink(dest);
+  await fs.copyFile(src, dest);
+
 })();

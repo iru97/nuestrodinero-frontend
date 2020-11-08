1c4fe0f2db7dc134b9153e43f14d56770c111fb2
franjpr <franpr90@gmail.com>
2020-11-08 08:46:19 +0000

    updated deployment

--- functions/index.js
+++
@@ -1,6 +0,0 @@
-const functions = require('firebase-functions');
-const universal = require(`${process.cwd()}/dist/nuestrodinero-frontend/server/main.js`);
-
-exports.ssr = functions.https.onRequest(universal.app());
-
-
